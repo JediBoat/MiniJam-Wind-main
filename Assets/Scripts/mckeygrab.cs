fcf30107e8491faa25763ad5ee8561c34bf5c4b8
JediBoat <121259910+JediBoat@users.noreply.github.com>
2024-03-31 19:10:00 +0100

    moreadditions

--- Assets/Scripts/mckeygrab.cs
+++ Assets/Scripts/mckeygrab.cs
@@ -6,12 +6,14 @@ public class mckeygrab : MonoBehaviour
 {
 
     public bool keygrabbed = false;
+    public Animator animator;
     
     private void OnTriggerEnter2D(Collider2D collider)
     {
         
         if (collider.gameObject.CompareTag("key"))
-        {
+        {   
+            animator.SetBool("keygot", true);
             keygrabbed = true;
             
         }
